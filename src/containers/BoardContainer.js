051543d7c4d118d35c09a80c3c593a6f777f7737
Tyler Cheswick <ttches@gmail.com>
2017-06-27 13:56:04 -0700

    Added win conditions

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -6,6 +6,7 @@ export default class BoardContainer extends Component {
   constructor(props) {
     super();
     this.handleTileClick = this.handleTileClick.bind(this);
+    this.checkWinner = this.checkWinner.bind(this);
     this.state = {
       players: 1,
       turn: 'X',
@@ -15,7 +16,7 @@ export default class BoardContainer extends Component {
 
   handleTileClick(e) {
     const index = e.target.dataset.index;
-    if (this.state.values[index]) return;
+    if (this.state.values[index] ) return;
     let newValues = [...this.state.values];
     newValues[index] = this.state.turn
     this.setState({
@@ -23,8 +24,25 @@ export default class BoardContainer extends Component {
       values: newValues,
       turn: (this.state.turn === 'X') ? 'O' : 'X'
     });
-    console.log(index);
   };
+
+  checkWinner(i) {
+    if (i === null) {
+      i = this.state.values;
+    }
+    if (i.slice(0, 3).every((value) => value === i[0] && i[0] !== null)) return true
+    if (i.slice(3, 6).every((value) => value === i[3] && i[3] !== null)) return true
+    if (i.slice(6, 9).every((value) => value === i[6] && i[6] !== null)) return true
+    if ([i[0], i[3], i[6]].every((value) => value === i[0] && i[0] !== null)) return true
+    if ([i[1], i[4], i[7]].every((value) => value === i[1] && i[1] !== null)) return true
+    if ([i[2], i[5], i[8]].every((value) => value === i[2] && i[2] !== null)) return true
+    if ([i[0], i[4], i[8]].every((value) => value === i[0] && i[0] !== null)) return true
+    if ([i[2], i[4], i[6]].every((value) => value === i[2] && i[2] !== null)) return true
+  }
+  componentDidUpdate() {
+    const i = this.state.values;
+    if (this.checkWinner(null)) console.log('winner');
+  }
 
   render() {
     return (
