9d34aaf1735000a2b96fa819b1c7b09107b4f890
Tyler Cheswick <ttches@gmail.com>
2017-06-29 12:49:09 -0700

    AI can play random tiles

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -5,26 +5,44 @@ import Board from '../components/Board';
 export default class BoardContainer extends Component {
   constructor(props) {
     super();
-    this.handleTileClick = this.handleTileClick.bind(this);
     this.checkWinner = this.checkWinner.bind(this);
+    this.getOpenTiles = this.getOpenTiles.bind(this);
+    this.handleAITurn = this.handleAITurn.bind(this);
+    this.handleTileClick = this.handleTileClick.bind(this);
+    this.playTile = this.playTile.bind(this);
+    this.toggleTurn = this.toggleTurn.bind(this);
     this.state = {
-      players: 1,
+      AI: 'O',
+      playing: true,
       turn: 'X',
       values: Array(9).fill(null)
     }
   }
 
+  //handles the users move
   handleTileClick(e) {
+    if (this.state.AI === this.state.turn || this.playing === false) return;  //If it's the computer's turn, return
     const index = e.target.dataset.index;
-    if (this.state.values[index] ) return;
+    if (this.state.values[index] ) return;  //If the tile is occupied, return
+    this.playTile(index);
+  };
+
+  handleAITurn() {
+    const delay = (Math.floor(Math.random() * 2) + 0.5) * 1000;
+    const openTiles = this.getOpenTiles();
+    const randomTile = openTiles[Math.floor(Math.random() * openTiles.length)];
+    setTimeout(() => this.playTile(randomTile), delay)
+  }
+
+  playTile(tile) {
     let newValues = [...this.state.values];
-    newValues[index] = this.state.turn
+    newValues[tile] = this.state.turn
     this.setState({
       ...this.state,
       values: newValues,
-      turn: (this.state.turn === 'X') ? 'O' : 'X'
+      turn: this.toggleTurn()
     });
-  };
+  }
 
   checkWinner(i) {
     if (i === null) {
@@ -39,9 +57,24 @@ export default class BoardContainer extends Component {
     if ([i[0], i[4], i[8]].every((value) => value === i[0] && i[0] !== null)) return true
     if ([i[2], i[4], i[6]].every((value) => value === i[2] && i[2] !== null)) return true
   }
+
+  getOpenTiles() {
+    return this.state.values.reduce((accu, value, i) => {
+      if (value === null) {
+        accu.push(i)
+      }
+      return accu;
+    }, [])
+  }
+
+  toggleTurn() {
+    return (this.state.turn === 'X') ? 'O' : 'X';
+  }
+
   componentDidUpdate() {
-    const i = this.state.values;
     if (this.checkWinner(null)) console.log('winner');
+    if (this.getOpenTiles().length === 0) console.log('tie');
+    if (this.state.AI === this.state.turn) this.handleAITurn();
   }
 
   render() {
