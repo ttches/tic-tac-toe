15c0c0a4d4b456f71939d28c571063d03b2dc470
Tyler Ches <ttches@gmail.com>
2017-07-01 15:39:54 -0700

    master AI works, seems unbeatable

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -9,12 +9,13 @@ export default class BoardContainer extends Component {
     this.getOpenTiles = this.getOpenTiles.bind(this);
     this.handleAITurn = this.handleAITurn.bind(this);
     this.handleTileClick = this.handleTileClick.bind(this);
-    this.masterAI = this.masterAI.bind(this);
+    this.miniMax = this.miniMax.bind(this);
     this.playTile = this.playTile.bind(this);
     this.toggleTurn = this.toggleTurn.bind(this);
     this.state = {
       AI: 'O',
-      playing: true,
+      AISkill: 'master', //manage this with a prop
+      playing: true, //manage this with a prop
       turn: 'X',
       values: Array(9).fill(null)
     }
@@ -22,41 +23,74 @@ export default class BoardContainer extends Component {
 
   //handles the users move
   handleTileClick(e) {
-    if (this.state.AI === this.state.turn || this.playing === false) return;  //If it's the computer's turn, return
+    //If it's the computer's turn, return
+    if (this.state.AI === this.state.turn || this.state.playing === false) return;
     const index = e.target.dataset.index;
-    if (this.state.values[index] ) return;  //If the tile is occupied, return
+    //If the tile is occupied, return
+    if (this.state.values[index] ) return;
     this.playTile(index);
   };
 
   handleAITurn() {
     const delay = (Math.floor(Math.random() * 2) + 0.5) * 1000;
-    const openTiles = this.getOpenTiles(); //array of open game spaces
-    const randomTile = openTiles[Math.floor(Math.random() * openTiles.length)]; //comment out once masterAI works
-    /*testing this with a console.log
-    I want an array of openTiles.length with a value of how strong
-    a move at each tile would be*/
-    console.log(openTiles, openTiles.map((tile) => {
-      let values = [...this.state.values];
-      values[tile] = this.state.turn;
-      return this.masterAI(values, this.state.turn, 0, this.state.turn);
-    }))
-    setTimeout(() => this.playTile(randomTile), delay) //comment out once masterAI works
+    const { AISkill } = this.state
+    //chooses master, basic, or intermediate AI
+    const fn = (AISkill === 'master')
+      ? this.masterAI.bind(this)
+      : (AISkill === 'basic')
+        ? this.basicAI.bind(this)
+        : console.log
+    setTimeout(() => fn(), delay);
   }
 
-  masterAI(values, turn, score, AISymbol) {
-    //base cases
-    console.log('Board at beginning of recursion: ', values);
-    if (this.checkWinner(values) === AISymbol) return score + 10;
-    if (this.checkWinner(values) === this.toggleTurn(AISymbol)) return score - 10;
-    if (this.getOpenTiles(values).length === 0) return score;
-    var openTiles = this.getOpenTiles(values);
-    console.log('Open tiles: ', openTiles);
-    openTiles.forEach((tile) => {
-      values[tile] = turn;
-      turn = this.toggleTurn(turn);
-      console.log('Board at end of recursion: ', values);
-      return score + this.masterAI(values, turn, score, AISymbol);
-    })
+  basicAI() {
+    //plays random tile
+    console.log(this)
+    const openTiles = this.getOpenTiles();
+    const randomTile = openTiles[Math.floor(Math.random() * openTiles.length)];
+    this.playTile(randomTile)
+  }
+
+  masterAI() {
+    const openTiles = this.getOpenTiles();
+    //minimax possible moves
+    const tileScores =
+      openTiles.map((tile) => {
+        let newValues = [...this.state.values];
+        newValues[tile] = this.state.turn;
+        return this.miniMax(newValues, this.state.turn, this.state.turn);
+      });
+    //choses a random best move if more than one exist
+    const bestTileScore = tileScores.reduce((accu, score) => (score > accu) ? score : accu, -11);
+    const bestTileScores = tileScores.reduce((accu, score, i) => {
+      if (score === bestTileScore) accu.push(i);
+      return accu;
+    }, []);
+    const choiceIndex = bestTileScores[Math.floor(Math.random() * bestTileScores.length)];
+
+    this.playTile(openTiles[choiceIndex]);
+  }
+
+  miniMax(values, turnState, AIturn) {
+    const openTiles = this.getOpenTiles(values);
+    turnState = this.toggleTurn(turnState);
+    if (this.checkWinner(values)) {
+      return (this.checkWinner(values) === AIturn) ? 10 : -10;
+    }
+    if(openTiles.length === 0) return 0;
+    let scores = []
+
+    for (let i = 0; i < openTiles.length; i++) {
+      let newValues = [...values];
+      newValues[openTiles[i]] = turnState;
+      scores.push(this.miniMax(newValues, turnState, AIturn));
+    }
+
+    if (turnState === AIturn) {
+      return scores.reduce((accu, score) => (score > accu) ? score : accu, -11)
+    } else {
+      return scores.reduce((accu, score) => (score < accu) ? score : accu, 11)
+    }
   }
 
   playTile(tile) {
@@ -78,6 +112,7 @@ export default class BoardContainer extends Component {
     if ([i[2], i[5], i[8]].every((value) => value === i[2] && i[2] !== null)) return i[2]
     if ([i[0], i[4], i[8]].every((value) => value === i[0] && i[0] !== null)) return i[0]
     if ([i[2], i[4], i[6]].every((value) => value === i[2] && i[2] !== null)) return i[2]
+    return false;
   }
 
   getOpenTiles(tiles = this.state.values) {
@@ -97,8 +132,10 @@ export default class BoardContainer extends Component {
     if (this.checkWinner()) console.log(this.checkWinner());
     if (this.getOpenTiles().length === 0) {
       console.log('tie');
+
+      //todo change playing state here
     }
-    if (this.state.AI === this.state.turn) {
+    if (this.state.AI === this.state.turn && this.state.playing === true) { //change playing to a prop
       this.handleAITurn();
     }
   }
